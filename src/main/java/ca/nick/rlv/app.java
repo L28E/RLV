a54eaecc5df06bdbcabdc9c974a53ac0fe0b75ad
L28E <nicholas.r@sympatico.ca>
2020-02-09 22:12:47 -0600

    Switched to a more vanilla JNA gphoto2 library, with a nice API.

--- src/main/java/ca/nick/rlv/app.java
+++ src/main/java/ca/nick/rlv/app.java
@@ -1,29 +1,38 @@
 package ca.nick.rlv;
 
-import x.mvmn.gphoto2.jna.Camera;
-import x.mvmn.gphoto2.jna.Camera.ByReference;
-import x.mvmn.gphoto2.jna.Gphoto2Library;
+import java.io.IOException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.angryelectron.gphoto2.GPhoto2;
 
 public class app {
 
 	public static void main(String[] args) {
-		final Camera camera = newCamera();
-
-	}
-	
-	public static ByReference newCamera() {
-		Camera.ByReference[] p2CamByRef = new Camera.ByReference[] { new Camera.ByReference() };
-		check(Gphoto2Library.INSTANCE.gp_camera_new(p2CamByRef));
-		return p2CamByRef[0];
-	}
-	
-	public static int check(int retVal) {
-		if (retVal < 0) {
-			System.err.println("Error " + retVal);
-			new Exception().printStackTrace();
+				
+		// Connect to camera
+		GPhoto2 camera = new GPhoto2();
+		try {
+			camera.open();
+		} catch (IOException ex) {
+			Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
+			System.exit(-1);
 		}
+		
+		// Diddle around with camera settings           
+        try {
+            String iso = camera.getConfig("iso"); // Where params are found by entering command "gphoto2 --list-config" in a terminal"
+            System.out.println(iso);            
+        } catch (IOException ex) {
+            Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
+            camera.close();
+            System.exit(-1);
+        }                                     
+        
+        // Cleanup
+        camera.close();
 
-		return retVal;
 	}
 
 }
