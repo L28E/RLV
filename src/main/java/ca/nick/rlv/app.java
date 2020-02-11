b31de8187eb82ff4f57a5098e3f85f6cb3d5f559
L28E <nicholas.r@sympatico.ca>
2020-02-10 23:09:41 -0500

    Test liveview function by getting a single frame

--- src/main/java/ca/nick/rlv/app.java
+++ src/main/java/ca/nick/rlv/app.java
@@ -1,16 +1,24 @@
 package ca.nick.rlv;
 
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import com.angryelectron.gphoto2.GPhoto2;
 
 public class app {
 
 	public static void main(String[] args) {
-				
+
 		// Connect to camera
 		GPhoto2 camera = new GPhoto2();
 		try {
@@ -19,19 +27,27 @@ public class app {
 			Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
 			System.exit(-1);
 		}
-		
-		// Diddle around with camera settings           
-        try {
-            String iso = camera.getConfig("iso"); // Where params are found by entering command "gphoto2 --list-config" in a terminal"
-            System.out.println(iso);            
-        } catch (IOException ex) {
-            Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
-            camera.close();
-            System.exit(-1);
-        }                                     
-        
-        // Cleanup
-        camera.close();
+
+		// Diddle around with the camera
+		try {
+//            String iso = camera.getConfig("iso"); // Where params are found by entering command "gphoto2 --list-config" in a terminal"
+//            System.out.println(iso);
+			BufferedImage image = camera.capturePreview();
+
+			JFrame frame = new JFrame();
+			frame.getContentPane().setLayout(new FlowLayout());
+			frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+			frame.pack();
+			frame.setVisible(true);
+
+		} catch (IOException ex) {
+			Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
+			camera.close();
+			System.exit(-1);
+		}
+
+		// Cleanup
+		camera.close();
 
 	}
 
