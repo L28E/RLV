2382ecf1fd95fc9264bc5fc5ff18dee30b14bdad
L28E <nicholas.r@sympatico.ca>
2020-02-23 20:40:21 -0500

    Use a multipart message to stream preview frames

--- src/main/java/ca/nick/rlv/app.java
+++ src/main/java/ca/nick/rlv/app.java
@@ -1,27 +1,28 @@
 package ca.nick.rlv;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-
+import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
 import com.angryelectron.gphoto2.GPhoto2;
 
 public class app {
-
 	private static GPhoto2 camera = new GPhoto2();
+	private static final byte[] prefix = ("--boundary\r\nContent-type: image/jpg\r\nContent-Length: ").getBytes();
+	private static final byte[] separator = "\r\n\r\n".getBytes();
 
 	public static void main(String[] args) throws Exception {
-		
+
+		// Establish a connection to the camera
 		try {
 			camera.open();
 		} catch (IOException ex) {
@@ -29,33 +30,50 @@ public class app {
 			System.exit(-1);
 		}
 
-		Server server = new Server(8080); // Instanciate server
-		ServletHandler handler = new ServletHandler(); // Instanciate handler
-		server.setHandler(handler); // Register handler with server object
-		handler.addServletWithMapping(Snap.class, "/snap"); // Mount the servlet class at some context path
-		server.start(); // Starts the server
-		server.join(); // current thread joins and waits for server thread to finish
-
+		// Start the server
+		Server server = new Server(8080);
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
+		handler.addServletWithMapping(StreamMJPG.class, "/stream.mjpg");
+		server.start();
+		server.join();
 	}
 
 	@SuppressWarnings("serial")
-	public static class Snap extends HttpServlet {
+	public static class StreamMJPG extends HttpServlet {
 
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						
-			try {				
-				BufferedImage img = camera.capturePreview();
-				resp.setStatus(HttpServletResponse.SC_OK);
-				resp.setContentType("image/jpg");
-				ImageIO.write(img, "jpg", resp.getOutputStream());
-			} catch (IOException ex) {
-				Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
-				// camera.setConfig("output", "TFT");
-				camera.close();
-				System.exit(-1);
+			final OutputStream out = resp.getOutputStream();
+			byte[] frame;	
+			
+			/*
+			 * The MIME content type "multipart/x-mixed-replace" lets me replace a component
+			 * of the page in subsequent HTTP responses.
+			 */
+			resp.setContentType("multipart/x-mixed-replace; boundary=--boundary");
+			resp.setStatus(HttpServletResponse.SC_OK);
+
+			// Send HTTP responses with frame data in a while loop.
+			while (true) {
+				try {
+					// Get a frame.
+					frame = camera.capturePreview();
+
+					// Write the HTTP response.
+					out.write(prefix);
+					out.write(String.valueOf(frame.length).getBytes());
+					out.write(separator);
+					out.write(frame);
+					out.write(separator);
+					out.flush();
+				} catch (IOException ex) {
+					Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
+					// camera.setConfig("output", "TFT");
+					camera.close();
+					System.exit(-1);
+				}
 			}
 		}
 	}
-
 }
