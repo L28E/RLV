6173d78158f19010bd43d5e40b11dfcadf884329
L28E <nicholas.r@sympatico.ca>
2020-02-25 00:05:06 -0500

    Added index.html
    img element has the mjpg serving servlet as a source. Camera controls
    will eventually be added.

--- src/main/java/ca/nick/rlv/app.java
+++ src/main/java/ca/nick/rlv/app.java
@@ -10,10 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.angryelectron.gphoto2.GPhoto2;
+import com.angryelectron.libgphoto2.Gphoto2Library.CameraFile;
 
 public class app {
 	private static GPhoto2 camera = new GPhoto2();
@@ -22,7 +29,7 @@ public class app {
 
 	public static void main(String[] args) throws Exception {
 
-		// Establish a connection to the camera
+		// Open a camera connection
 		try {
 			camera.open();
 		} catch (IOException ex) {
@@ -30,13 +37,34 @@ public class app {
 			System.exit(-1);
 		}
 
-		// Start the server
 		Server server = new Server(8080);
-		ServletHandler handler = new ServletHandler();
-		server.setHandler(handler);
-		handler.addServletWithMapping(StreamMJPG.class, "/stream.mjpg");
+		HandlerList handlers = new HandlerList();
+
+		// Resource handler
+		ResourceHandler resourceHandler = new ResourceHandler();
+		resourceHandler.setDirectoriesListed(true);
+		resourceHandler.setResourceBase(".");
+		resourceHandler.setWelcomeFiles(new String[] { "index.html" });
+
+		// Image handler
+		ServletContextHandler context = new ServletContextHandler();
+		context.setContextPath("/");
+		context.addServlet(StreamMJPG.class, "/stream.mjpg");
+
+		// Register handlers with the server
+		handlers.setHandlers(new Handler[] { resourceHandler, context, new DefaultHandler() });
+		server.setHandler(handlers);
+
+		// Start server
 		server.start();
 		server.join();
+
+		// Close the camera connection when the program ends
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				camera.close();
+			}
+		});
 	}
 
 	@SuppressWarnings("serial")
@@ -46,6 +74,7 @@ public class app {
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			final OutputStream out = resp.getOutputStream();
 			byte[] frame;
+			CameraFile fileRef = camera.createCameraFile();
 
 			/*
 			 * The MIME content type "multipart/x-mixed-replace" is a type of HTTP response
@@ -55,10 +84,10 @@ public class app {
 			resp.setStatus(HttpServletResponse.SC_OK);
 
 			// Send messages with frame data in a while loop.
-			while (true) {
-				try {
+			try {
+				while (true) { // TODO: Find a logical exit condition.
 					// Get a frame.
-					frame = camera.capturePreview();
+					frame = camera.capturePreview(fileRef);
 
 					// Write the message.
 					out.write(prefix);
@@ -67,13 +96,18 @@ public class app {
 					out.write(frame);
 					out.write(separator);
 					out.flush();
-				} catch (IOException ex) {
-					Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
-					// camera.setConfig("output", "TFT");
-					camera.close();
-					System.exit(-1);
+					
+					// TODO: This kinda stinks, try un-reffing stuff here and in the api.
+					System.gc();
 				}
+			} catch (IOException ex) {
+				Logger.getLogger(app.class.getName()).log(Level.SEVERE, ex.getMessage());
+				camera.close();
+				System.exit(-1);
+			} finally {
+				camera.close();
 			}
 		}
+
 	}
 }
